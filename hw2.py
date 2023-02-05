67867c7c292a53da245f15fbe7ae3565a7b28c85
Gayusaraksh <gayusaraksh@gmail.com>
2023-02-05 11:16:47 -0600

    Locators

---
+++ hw2.py
@@ -0,0 +1,16 @@
+"""
+Amazon logo- (By xpath,"//i[@aria-label='Amazon']")
+Sign-in-     (By xpath,"//h1[@class='a-spacing-small']")
+Email or mobile phone number(title)  - (By xpath,"//label[@for='ap_email']")
+Email or mobile phone number(text box) - ( By xpath,"//input[@type='email' and @class='a-input-text a-span12 auth-autofocus auth-required-field']")
+Continue button-  (By xpath,"//input[contains(@id,'continue' )and @aria-labelledby='continue-announce']")
+Conditions of use link - (By xpath, "//a[text()='Conditions of Use']")
+Privacy Notice link - (By xpath, "//a[text()='Privacy Notice']")
+Need help link - (By xpath, "//span[@class='a-expander-prompt']")
+Forgot your password? link - (By xpath, "//a[@id='auth-fpp-link-bottom']")
+Other issues with sign-in link - (By xpath, "//a[@id='ap-other-signin-issues-link']")
+Create your amazon account button - (By xpath, "//a[@id='createAccountSubmit']")
+Privacy Notice link (below) - (By xpath, "//a[@class='a-link-normal' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496']")
+Conditions of Use link (below) - (By xpath, "//a[@class='a-link-normal' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088']")
+Help link (below) - (By xpath, "//a[@class='a-link-normal' and @target='_blank' and @rel='noopener' and @href='/help']"
+"""
