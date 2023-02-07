3095665f85c6d689db989f15eff0640f87d7071d
Gayusaraksh <gayusaraksh@gmail.com>
2023-02-07 14:56:49 -0600

    Lesson2hw

--- hw2.py
+++ hw2.py
@@ -1,16 +1,43 @@
 """
-Amazon logo- (By xpath,"//i[@aria-label='Amazon']")
-Sign-in-     (By xpath,"//h1[@class='a-spacing-small']")
-Email or mobile phone number(title)  - (By xpath,"//label[@for='ap_email']")
-Email or mobile phone number(text box) - ( By xpath,"//input[@type='email' and @class='a-input-text a-span12 auth-autofocus auth-required-field']")
-Continue button-  (By xpath,"//input[contains(@id,'continue' )and @aria-labelledby='continue-announce']")
-Conditions of use link - (By xpath, "//a[text()='Conditions of Use']")
-Privacy Notice link - (By xpath, "//a[text()='Privacy Notice']")
-Need help link - (By xpath, "//span[@class='a-expander-prompt']")
-Forgot your password? link - (By xpath, "//a[@id='auth-fpp-link-bottom']")
-Other issues with sign-in link - (By xpath, "//a[@id='ap-other-signin-issues-link']")
-Create your amazon account button - (By xpath, "//a[@id='createAccountSubmit']")
-Privacy Notice link (below) - (By xpath, "//a[@class='a-link-normal' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496']")
-Conditions of Use link (below) - (By xpath, "//a[@class='a-link-normal' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088']")
-Help link (below) - (By xpath, "//a[@class='a-link-normal' and @target='_blank' and @rel='noopener' and @href='/help']"
+Amazon logo:
+ driver.find_element(By.XPATH,"//i[@aria-label='Amazon']")
+
+Sign-in:
+ driver.find_element(By.XPATH,"//h1[@class='a-spacing-small']")
+
+Email or mobile phone number(title):
+ driver.find_element(By.XPATH,"//label[@for='ap_email']")
+
+Email or mobile phone number(text box):
+ driver.find_element( By.ID,'ap_email')
+
+Continue button:
+ driver.find_element(By.ID,'continue')
+
+Conditions of use link:
+ driver.find_element(By.XPATH, "//a[contains(@href,'/ref=ap_signin_notification_condition_of_use?')]")
+
+Privacy Notice link:
+ driver.find_element(By.XPATH, "//a[contains(@href,'/ref=ap_signin_notification_privacy_notice?')]")
+
+Need help link:
+ driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+Forgot your password? link:
+ driver.find_element(By.ID,"auth-fpp-link-bottom")
+
+Other issues with sign-in link:
+ driver.find_element(By.ID,'ap-other-signin-issues-link')
+
+Create your amazon account button:
+ driver.find_element(By.ID,'createAccountSubmit')
+
+Privacy Notice link (footer):
+ driver.find_element(By.XPATH, "//a[contains(@href,'/ref=ap_desktop_footer_privacy_notice?') and  @class='a-link-normal']"
+
+Conditions of Use link (footer):
+ driver.find_element(By.XPATH, "//a[contains(@href,'/ref=ap_desktop_footer_cou?') and @class='a-link-normal']"
+
+Help link (footer):
+ driver.find_element(By.XPATH, "//a[@class='a-link-normal' and @target='_blank' and @rel='noopener' and @href='/help']"
 """
